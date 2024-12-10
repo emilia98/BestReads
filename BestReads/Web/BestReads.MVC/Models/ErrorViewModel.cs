5522a5fd1462a68024622b643c6293e9f4eef3d2
emilia98 <emilia_nedialkova@abv.bg>
2024-12-10 19:11:05 +0200

    setup the project

---
+++ BestReads/Web/BestReads.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BestReads.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
