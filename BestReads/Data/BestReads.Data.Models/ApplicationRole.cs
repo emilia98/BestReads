f5ee527eaf1893f38beab4d186055632d9f87a3a
emilia98 <emilia_nedialkova@abv.bg>
2024-12-11 20:14:03 +0200

    add ApplicationUser and ApplicationRole models

---
+++ BestReads/Data/BestReads.Data.Models/ApplicationRole.cs
@@ -0,0 +1,22 @@
+﻿using BestReads.Data.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BestReads.Data.Models
+{
+    public class ApplicationRole : IdentityRole<int>, IAuditInfo, IDeletableEntity
+    {
+        public ApplicationRole() : this(null)
+        { }
+
+        public ApplicationRole(string name) : base(name)
+        { }
+
+        public DateTime CreatedAt { get; set; }
+        
+        public DateTime? ModifiedAt { get; set; }
+        
+        public bool IsDeleted { get; set; }
+        
+        public DateTime? DeletedAt { get; set ; }
+    }
+}
---
+++ BestReads/Data/BestReads.Data.Models/ApplicationUser.cs
@@ -0,0 +1,29 @@
+﻿using BestReads.Data.Common.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace BestReads.Data.Models
+{
+    public class ApplicationUser : IdentityUser<int>, IAuditInfo, IDeletableEntity
+    {
+        public ApplicationUser()
+        {
+            this.Roles = new HashSet<IdentityUserRole<int>>();
+            this.Claims = new HashSet<IdentityUserClaim<int>>();
+            this.Logins = new HashSet<IdentityUserLogin<int>>();
+        }
+
+        public DateTime CreatedAt { get; set; }
+        
+        public DateTime? ModifiedAt { get; set; }
+        
+        public bool IsDeleted { get; set; }
+        
+        public DateTime? DeletedAt { get; set; }
+
+        public virtual ICollection<IdentityUserRole<int>> Roles { get; set; }
+
+        public virtual ICollection<IdentityUserClaim<int>> Claims { get; set; }
+
+        public virtual ICollection<IdentityUserLogin<int>> Logins { get; set; }
+    }
+}
